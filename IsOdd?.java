a5782f10019e64bea54679014c413992898e279d
vijayaF2 <119723559+vijayaF2@users.noreply.github.com>
2023-08-23 20:07:33 +0530

    import java.util.Scanner; public class Odd{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n%2!=0)         {             System.out.println("2");         }         else         {             System.out.println("1");         }     } }

---
+++ IsOdd?.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Odd{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2!=0)
+        {
+            System.out.println("2");
+        }
+        else
+        {
+            System.out.println("1");
+        }
+    }
+}
