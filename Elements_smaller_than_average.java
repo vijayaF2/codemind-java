6f1a869872ef14f21459557bbd40a9bc3d99db4a
vijayaF2 <119723559+vijayaF2@users.noreply.github.com>
2023-08-26 10:48:57 +0530

    import java.util.Scanner; public class Average{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] integerArray=new int[n];         for(int i=0; i<n; i++)         {             integerArray[i]=sc.nextInt();         }         int sum=0;         int c=0;         for(int i=0; i<n;i++)         {             sum+=integerArray[i];         }         int average=sum/n;         for(int i=0; i<n; i++)         {             if(integerArray[i]<=average)             {                 c+=1;             }         }         System.out.println(c);
        }
    }

---
+++ Elements_smaller_than_average.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Average{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] integerArray=new int[n];
+        for(int i=0; i<n; i++)
+        {
+            integerArray[i]=sc.nextInt();
+        }
+        int sum=0;
+        int c=0;
+        for(int i=0; i<n;i++)
+        {
+            sum+=integerArray[i];
+        }
+        int average=sum/n;
+        for(int i=0; i<n; i++)
+        {
+            if(integerArray[i]<=average)
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+        
+    }
+}
