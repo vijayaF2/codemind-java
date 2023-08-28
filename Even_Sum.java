54c9c755bfec8e269fbebd4cefe77700486ad723
vijayaF2 <119723559+vijayaF2@users.noreply.github.com>
2023-08-28 19:51:10 +0530

    import java.util.Scanner; public class Even{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0; i<n; i++)         {             a[i]=sc.nextInt();         }         int sum=0;         for(int i=0; i<n; i++)         {             if(a[i]%2==0)             {               sum+=a[i];             }         }         System.out.println(sum);     } }

--- Even_Sum.java
+++ Even_Sum.java
@@ -1,20 +1,20 @@
-import java.util.*;
-public class EvenSum{
+import java.util.Scanner;
+public class Even{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int[] intArray=new int[n];
-        for(int i=0;i<n;i++)
+        int[] a=new int[n];
+        for(int i=0; i<n; i++)
         {
-            intArray[i]=sc.nextInt();
+            a[i]=sc.nextInt();
         }
         int sum=0;
-        for(int i=0;i<n;i++)
+        for(int i=0; i<n; i++)
         {
-            if(intArray[i]%2==0)
+            if(a[i]%2==0)
             {
-                sum+=intArray[i];
+              sum+=a[i];
             }
         }
         System.out.println(sum);
