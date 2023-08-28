0f442bcc66110db7edab16cd5b9f7ba56dc54c88
vijayaF2 <119723559+vijayaF2@users.noreply.github.com>
2023-08-28 19:39:37 +0530

    import java.util.Scanner; public class Even{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0; i<n; i++)         {             a[i]=sc.nextInt();         }         int sum=0;         for(int i=0; i<n; i++)         {             if(i%2==0)             {                 sum+=a[i];             }         }         System.out.println(sum);     } }

---
+++ Sum_of_Even_Positions.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Even{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0; i<n; i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0; i<n; i++)
+        {
+            if(i%2==0)
+            {
+                sum+=a[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
